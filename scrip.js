4f8813b5696b5eb2fc69d06a51ccef5eee89cd52
Johongir10 <shavkatovj73@gmail.com>
2024-07-29 10:15:48 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,12 @@
+let son=+prompt("Birdan 7 gacha son kiriting").toLocaleLowerCase().trim();
+
+switch(son){
+    case 1:alert("Dushanba");break; 
+    case 2:alert("Seshanba");break;
+    case 3:alert("chorshanba");break;
+    case 4:alert("Payshanba");break;
+    case 5:alert("Juma");break;
+    case 6:alert("shanba");break;
+    case 7:alert("Yakshanba");break;
+    default: alert("Bunday son yoq");break;
+}
